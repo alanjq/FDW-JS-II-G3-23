3616383053284435ca1688631cadffca1b3768ed
Alan Jimenez <alanjq@outlook.com>
2023-09-25 23:00:16 -0500

    Webpack HTML plugin

---
+++ semana-1/tema-1/ejemplo-jsportfolio/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        // Generar los archivos para subir al servidor dentro de la carpeta "salida"
+        path: path.resolve(__dirname, 'salida'),
+        filename: 'resultado.js',
+    },
+    resolve: {
+        extensions: ['.js']
+    },
+    plugins: [
+        // Permitimos usar HTML dentro
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: "./public/index.html",
+            filename: "./index.html"
+        })
+    ]
+}
