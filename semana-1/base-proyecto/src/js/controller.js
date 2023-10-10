0a3ca3be77837a93a52b64a31b6ea2a28ec8b103
Alan Jimenez <alanjq@outlook.com>
2023-10-09 22:07:43 -0500

    Avance Semana 1

--- semana-1/base-proyecto/src/js/controller.js
+++ semana-1/base-proyecto/src/js/controller.js
@@ -1,3 +1,8 @@
+import icons from "../img/icons.svg"
+import { ingredientList } from "./ingredientList";
+import { recipeDetailsMarkup } from "./recipe";
+import { renderSpinner } from "./spinner";
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -11,3 +16,27 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+
+async function showRecipe(url) {
+  renderSpinner(recipeContainer)
+  const response = await fetch(url)
+  
+  const { data } = await response.json()
+  
+  const { recipe } = data
+  
+  return (recipe)
+}
+
+const URL_API = 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+showRecipe(URL_API)
+.then((recipe) => {
+  // Colocamos el HTML
+    recipeContainer.insertAdjacentHTML('afterbegin', recipeDetailsMarkup(recipe))
+    recipeContainer.insertAdjacentHTML('afterbegin', ingredientList(recipe))
+    console.log(recipe);
+  })
+  .catch((er) => {
+    console.error('URL mal', er)
+  })
